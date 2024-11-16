37aadcb4c475793d9dafc03846bde8314d32cb7a
Adilet72 <adiletashimov08@gmail.com>
2024-11-16 13:03:11 +0600

    first

--- app.js
+++ app.js
@@ -24,6 +24,59 @@ const menu = [
 ]
 
 const menuEl = document.getElementById('menu')
+const recipes_buttons = document.querySelector('.recipes-buttons')
+const recipes_cards = document.querySelector('.recipes-cards')
+
+const buttonsName = [
+    "Starter",
+    "Beef",
+    "Breakfast",
+    "Chicken",
+    "Pasta",
+    "Dessert",
+    "Vegan",
+]
+let btnIndex = 0
+
+let url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c='
+
+function renderBtn(){
+    recipes_buttons.innerHTML = buttonsName.map( (btn ,index)=> {
+        let clas = btnIndex == index ? "active" : ""
+        return `<button class="${clas}">${btn}</button>`
+    }).join("")
+    recipes_buttons.querySelectorAll('button').forEach( (btn, i )=> {
+        btn.addEventListener('click', () => {
+            fetch(url + btn.innerText)
+            .then(res => res.json())
+            .then( ( {meals} ) => {
+                console.log("meals", meals);
+                recipes_cards.innerHTML = meals.map(f => {
+                    return` <div class="card">
+                    <div class="card-img">
+                        <img src="./img/Recipe Image.svg" alt="">
+                    </div>
+                    <div class="card-text">
+                        <h4>Savory Herb-Infused Chicken</h4>
+                        <p>Indulge in the rich and savory symphony of flavors with our Savory Herb-Infused Chicken</p>
+                        <div>
+                            <p>40 Min - easy prep - 3 serves</p>
+                            <button>view recipe</button>
+                        </div>
+                    </div>
+                </div>`
+                })
+            })
+            btnIndex = i
+            renderBtn()
+            
+        })
+    })
+}
+
+renderBtn()
+
+
 
 menuEl.innerHTML = menu.map( m => {
     return`
